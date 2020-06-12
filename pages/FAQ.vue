4aca6438bc7aea3ae9d637dd7bc3b2be987fb4fc
James <jamestedesco802@gmail.com>
2020-06-12 02:46:58 -0400

    Added link to admin dashboard

--- pages/FAQ.vue
+++ pages/FAQ.vue
@@ -1,23 +1,31 @@
 <template>
-  <v-card>
-    <v-card-title>Contact Info</v-card-title>
-    <v-card-text class="subtitle-1">
-      Our country hasn't
-      <em>just suddenly</em> transformed into racist mayhem, it's been going steady since its earliest days. It's my hope that this website can be a platform for
-      uplifting business owners who are faced with many systemic oppressions.
-      If something about this site isn't working or you feel confused, don't hestitate to
-      <a target="_blank"
-        href="https://www.jamestedesco.me/#contact"
-      >reach out</a>.
-    </v-card-text>
+  <v-layout column>
+    <v-card class="my-5">
+      <v-card-title>How do I edit my page?</v-card-title>
+      <v-card-text>There's an <nuxt-link to='/admin'>Admin Dashboard</nuxt-link> where you can edit pages in a way similar to social media. The portal will require you to register, but there are no special requirements for that.</v-card-text>
+    </v-card>
+    <v-card class="my-5">
+      <v-card-title>Contact</v-card-title>
+      <v-card-text class="subtitle-1">
+        Our country hasn't
+        <em>just suddenly</em> transformed into racist mayhem, it's been going steady since its earliest days. It's my hope that this website can be a platform for
+        uplifting business owners who are faced with many systemic oppressions.
+        If something about this site isn't working or you feel confused, don't hestitate to
+        <a
+          target="_blank"
+          href="https://www.jamestedesco.me/#contact"
+        >reach out</a>.
+      </v-card-text>
 
-    <v-card-text class="subtitle-1">
-      What's more, if you have experience programming with Vue or Nuxt, here's a link to the
-      <a target="_blank"
-        href="https://github.com/JamesTedesco/Vermont-BIPOC-Businesses"
-      >developer resources</a>.
-    </v-card-text>
-  </v-card>
+      <v-card-text class="subtitle-1">
+        What's more, if you have experience programming with Vue or Nuxt, here's a link to the
+        <a
+          target="_blank"
+          href="https://github.com/JamesTedesco/Vermont-BIPOC-Businesses"
+        >developer resources</a>.
+      </v-card-text>
+    </v-card>
+  </v-layout>
 </template>
 
 <script>
