6a796a92ea734e229e4dbe967cd523ed7a6cfd0d
James <jamestedesco802@gmail.com>
2020-06-12 00:35:24 -0400

    Added contact page

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,13 @@
       <nuxt-link class="unstyled-link" to="/">
         <v-toolbar-title v-text="title" />
       </nuxt-link>
+      <v-spacer></v-spacer>
+      <v-btn class="white mr-5">
+        <nuxt-link class="unstyled-link" to="/">Home</nuxt-link>
+      </v-btn>
+      <v-btn class="white">
+        <nuxt-link class="unstyled-link" to="/Contact">Contact</nuxt-link>
+      </v-btn>
     </v-app-bar>
 
     <!-- catch injected content -->
@@ -22,11 +29,10 @@
 
 <script>
 export default {
-
   head: {
     title: 'Vermont BIPOC Businesses',
     script: [
-      { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
     ]
   },
 
@@ -45,12 +51,11 @@ export default {
 
 .page-enter-active,
 .page-leave-active {
-  transition: all .2s ease;
+  transition: all 0.2s ease;
 }
 .page-enter,
 .page-leave-to {
   opacity: 0;
   transform: translateX(100vw);
 }
-
 </style>
---
+++ pages/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-title>Contact Info</v-card-title>
+    <v-card-text class="subtitle-1">
+      Our country hasn't
+      <em>just suddenly</em> transformed into racist mayhem, it's been going steady since its earliest days. It's my hope that this website can be a platform for
+      uplifting business owners who are faced with many systemic oppressions.
+      If something about this site isn't working or you feel confused, don't hestitate to
+      <a target="_blank"
+        href="https://www.jamestedesco.me/#contact"
+      >reach out</a>.
+    </v-card-text>
+
+    <v-card-text class="subtitle-1">
+      What's more, if you have experience programming with Vue or Nuxt, here's a link to the
+      <a target="_blank"
+        href="https://github.com/JamesTedesco/Vermont-BIPOC-Businesses"
+      >developer resources</a>.
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
